42808c6d9fdafa9edad04580d807420ae6556065
urodev <ourcssdev@gmail.com>
2023-03-04 20:47:15 -0600

    custom hooks - useCounter, useFetch, useForm, useTodos

---
+++ useTodos.js
@@ -0,0 +1,45 @@
+import { useEffect, useReducer } from 'react'
+import { todoReducer } from '../08-useReducer/todoReducer'
+
+const init = () => {
+  return JSON.parse(localStorage.getItem('todos')) || []
+}
+
+export const useTodos = () => {
+  const [todos, dispatchTodos] = useReducer(todoReducer, [], init)
+
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos))
+  }, [todos])
+
+  const handleNewTodo = (todo) => {
+    const action = {
+      type: 'add todo',
+      payload: todo
+    }
+    dispatchTodos(action)
+  }
+
+  const handleRemoveTodo = (id) => {
+    dispatchTodos({
+      type: 'rem todo',
+      payload: id
+    })
+  }
+
+  const handleToggleTodo = (id) => {
+    dispatchTodos({
+      type: 'toggle todo',
+      payload: id
+    })
+  }
+
+  return {
+    todos,
+    todosCount: todos.length,
+    pendingTodosCount: todos.filter((todo) => !todo.done).length,
+    handleNewTodo,
+    handleRemoveTodo,
+    handleToggleTodo
+  }
+}
